8c177d7db57189f6f517029fc3412db9252afd4b
Mitch Anderson <mitch@nuvi.com>
2015-12-26 14:20:49 -0700

    adding some unit tests via a the pytest framework

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 
 import re
+import os
 from setuptools import setup
 
 
@@ -8,10 +9,17 @@ version = re.search(
     open('yokedclient/cli.py').read()
     ).group(1)
 
+if not version:
+    raise RuntimeError('Cannot find version information')
 
 with open("README.rst", "rb") as f:
     long_descr = f.read().decode("utf-8")
 
+with open('requirements/common.txt') as f:
+    requirements = [r.strip() for r in f.readlines()]
+
+with open('requirements/test.txt') as f:
+    test_requirements = [r.strip() for r in f.readlines()]
 
 setup(name='yoked-client',
       version=version,
@@ -23,10 +31,10 @@ setup(name='yoked-client',
       license='MIT',
       packages=['yokedclient'],
       zip_safe=False,
-      install_requires=[
-          'requests==2.8.1',
-      ],
+      install_requires=requirements,
       entry_points = {
           'console_scripts': ['yokedctl = yokedclient.cli:main'],
-          }
+          },
+      test_suite='tests',
+      tests_require=test_requirements
       )
